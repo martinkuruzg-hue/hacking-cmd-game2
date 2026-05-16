029c7a948a5537bfcb6fe7ac2660dae4cda31e5a
ma.gutierrezk <martin.kuruzg@gmail.com>
2026-05-16 02:08:59 -0400

    Primera alpha

---
+++ node_2d.gd
@@ -0,0 +1,59 @@
+extends Node2D
+
+var texto : String
+var veces :int
+var lista_comandos = ['cd','cd .']
+var clear = ["clear", "cls"]
+var puntos: int
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	get_node("Label3").text = str(puntos)
+	if Input.is_action_just_pressed("ui_accept"):
+		cmd_base()
+	
+	if puntos <0:
+		puntos = 0
+
+
+func _on_button_pressed() -> void:
+	cmd_base()
+func cmd_base():
+		if $LineEdit.text in lista_comandos and not $LineEdit.text in clear:
+			llenar_cmd()
+			sumar_puntos()
+		elif $LineEdit.text in clear:
+			sumar_puntos()
+			
+			vaciar_cmd()
+		else:
+			restar_puntos()
+			input_invalido()
+		#llenar_cmd()
+		vaciar_input()
+func sumar_puntos():
+	puntos = puntos +1
+func restar_puntos():
+	puntos = puntos -1
+func vaciar_input():
+	$LineEdit.text = ""
+func llenar_cmd():
+	$Label.text =str($Label.text) + "\n" +str($LineEdit.text)
+func vaciar_cmd():
+	get_node("Label").text = ""
+	veces = 0
+func input_invalido():
+	$Label.text =str($Label.text) + "\n"+"'" +str($LineEdit.text)+"' command is invalid"
+func _on_label_resized() -> void:
+	
+	if veces <= 18:
+		veces = veces + 1
+		print(veces)
+	else:
+		veces = 0
+		get_node("Label").text =""
