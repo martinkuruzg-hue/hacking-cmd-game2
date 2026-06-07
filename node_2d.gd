f831c57f1520f661e6a248888eb44e8a51f05e80
ma.gutierrezk <martin.kuruzg@gmail.com>
2026-06-07 17:32:22 -0400

    alpha 7

--- node_2d.gd
+++ node_2d.gd
@@ -1,25 +1,22 @@
 extends Node2D
-#Declarar la variable de input
-# para entedimiento humano
-@onready var input = $LineEdit
+
 var texto : String
 var veces :int
-var enrutamiento : String
 #lista para comandos CORRECTOS que SUMAN PUNTOS
-var lista_comandos = ['cd','cd .','cd ~']
+#var lista_comandos = ['cd','cd .','cd ..','cd ~','cp','ls','mkdir']
+var lista_comandos = {'cd':1,'cd .':2,'cd ..':3,'cd ~':4,'cp':5,'ls':6}
  #COMANDOS para CERRAR TERMINAL
-var clear = ["clear", "cls"]
+var clear = {"clear":1, "cls":1}
 #VARIABLE PUNTOS (cuando se utilizan "lista_comandos" AUMENTA)
 var puntos: int
-# input = input
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	pass # Replace with function body.
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
 	#LABEL DE PUNTOS (Indicador de puntaje en PANTALLA)
 	get_node("Label3").text = str(puntos)
 	if Input.is_action_just_pressed("ui_accept"):
@@ -36,16 +33,18 @@ func _on_button_pressed() -> void:
 func cmd_base():
 		#SI EL COMANDO DE LA ENTRADA DE TEXTO
 		#ESTA EN LA LISTA DE COMANDOS CORRECTOS
-		if input.text in lista_comandos and not input.text in clear:
+		if $LineEdit.text in lista_comandos and not $LineEdit.text in clear:
 			#ENVIA EL COMANDO ESCRITO AL TERMINAL
 			llenar_cmd()
 			#SUMA PUNTOS (deberia variar segun dificultad de comando)
-			sumar_puntos()
+			#sumar_puntos()
+			sumar_puntos(lista_comandos,$LineEdit.text)
 			
 		#SI es un comando para CERRAR TERMINAL
-		elif input.text in clear:
+		elif $LineEdit.text in clear:
 			#SUMA PUNTOS (deberia ser 1)
-			sumar_puntos()
+			#sumar_puntos()
+			sumar_puntos(clear,$LineEdit.text)
 			#VACIA TERMINAL
 			vaciar_cmd()
 		#SI es COMANDO INCORRECTO X
@@ -60,18 +59,19 @@ func cmd_base():
 		# ESCRIBIR OTRO COMANDO
 		vaciar_input()
 #SUMA con LISTA_COMANDOS (COMANDOS CORRECTOS)
-func sumar_puntos():
+func sumar_puntos(a,b):
 	#A futuro que cambie la suma segun dificultad
-	puntos = puntos +1
+	puntos += a[b]
+	print(a[b])
 #RESTA con COMANDOS INCORRECTOS (-1 punto siempre)
 func restar_puntos():
 	puntos = puntos -1
 #FUNCION para VACIAR LA ENTRADA DE TEXTO
 func vaciar_input():
-	input.text = ""
+	$LineEdit.text = ""
 #ENVIA el texto del INPUT al TERMINAL
 func llenar_cmd():
-	$Label.text =str($Label.text) + "\n" +str(input.text)
+	$Label.text =str($Label.text) + "\n" +str($LineEdit.text)
 #VACIA TERMINAL cuando se LLENA
 func vaciar_cmd():
 	get_node("Label").text = ""
@@ -79,13 +79,13 @@ func vaciar_cmd():
 #CONCATENA mensaje de error , con comando fuera 
 # de lista_comandos
 func input_invalido():
-	$Label.text =str($Label.text) + "\n"+ enrutamiento +"'" +str(input.text)+"' command is invalid"
+	$Label.text =str($Label.text) + "\n"+"'" +str($LineEdit.text)+"' command is invalid"
 #REINICIA TERMINAL CUANDO SE LLENA
 func _on_label_resized() -> void:
 	#TERMINAL acepta MAXIMO 18 FILAS
 	if veces <= 18:
 		veces = veces + 1
-		print(veces)
+		#print(veces)
 	#CUANDO SUPERA LAS 18 FILAS
 	else:
 		#REINICIA RECUENTO A 0
