4fabf7cd060146f5f76a9ccb9ebe59b46c88ae6b
ma.gutierrezk <martin.kuruzg@gmail.com>
2026-06-13 11:52:32 -0400

    alpha 13

--- facil.gd
+++ facil.gd
@@ -11,11 +11,11 @@ var puntos: int
 var lista_carpetas = ["home"]
 var jerarquia_carpetas = {"home":0 , "Documents":1, "Desktop":1, "Download":1, "a":2,"e":3}
 var fichero = "~\\home>"
-var flojera = [1,2,3]
+var optimizacion = [1,2,3]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	flojera2()
+	optimizacion2()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -23,19 +23,19 @@ func _process(delta: float) -> void:
 
 
 func _on_button_2_pressed() -> void:
-	flojera2()
+	optimizacion2()
 	$Label.text =$Button2.text
 
 
 func _on_button_3_pressed() -> void:
-	flojera2()
+	optimizacion2()
 	$Label.text =$Button3.text
 
 func _on_button_1_pressed() -> void:
-	flojera2()
+	optimizacion2()
 	$Label.text =$Button1.text
 	
-func flojera2():
-	for i in flojera :
+func optimizacion2():
+	for i in optimizacion :
 		get_node("Button"+str(i)).text= str(lista_comandos.find_key(randi_range(1,8)))
 	
